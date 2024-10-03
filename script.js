71233e764ddcafec5d15e684a51ab9e6d328eae4
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-10-03 15:53:29 +0200

    Updated and fixed dark mode functionality

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ function darkMode() {
   const firstButton = document.getElementById("first-button");
   const secondButton = document.getElementById("second-button");
 
-  if (
-    firstButton.innerHTML === "Dark Mode" &&
-    secondButton.innerHTML === "Dark Mode"
-  ) {
+  if (element.classList.contains("dark-mode")) {
     firstButton.innerHTML = "Light Mode";
     secondButton.innerHTML = "Light Mode";
   } else {
     firstButton.innerHTML = "Dark Mode";
     secondButton.innerHTML = "Dark Mode";
   }
+}
+
+function colourButton() {
+  const colourButton = document.getElementById("colour-button");
+  const element = document.body;
+  element.classList.toggle("colourful-mode");
 }
